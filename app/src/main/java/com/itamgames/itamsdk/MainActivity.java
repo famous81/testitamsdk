46e32d2afd8cbac44ad8e750b5143bd16ce6a1ef
yoojim <famous@itam.games>
2019-04-02 15:47:22 +0900

    ...

--- app/src/main/java/com/itamgames/itamsdk/MainActivity.java
+++ app/src/main/java/com/itamgames/itamsdk/MainActivity.java
@@ -1,4 +1,15 @@
 package com.itamgames.itamsdk;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ItamSdk sdk;
+    }
 }
